c91461e938231d17344054e9d50df11acd3478b4
Ryan Knauer <ryanknauer@alumni.ubc.ca>
2019-07-16 15:01:54 -0700

    paths

--- a1-backend/app.js
+++ a1-backend/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +16,17 @@ app.use(express.static(path.join(__dirname + '/../client/build')))
 /*React root*/
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+});
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+  "Access-Control-Allow-Headers",
+  "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  
+  next();
 });
 app.options("*", cors());
 
